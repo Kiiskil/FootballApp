4d945cda792bfdf75157d1bd558212e15c149f33
Iiro Laukkanen <iirol@IironMasiina.iirola>
2019-11-17 12:14:03 +0200

    Refactored controller

--- FootballAPI/Controllers/FootballMatchController.cs
+++ FootballAPI/Controllers/FootballMatchController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using FootballAPI.Models;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace FootballAPI.Controllers
 {
@@ -12,16 +15,41 @@ namespace FootballAPI.Controllers
     public class FootballMatchController : ControllerBase
     {
         private readonly ILogger<FootballMatchController> _logger;
+        Matchlist _matches = new Matchlist();
 
         public FootballMatchController(ILogger<FootballMatchController> logger)
         {
             _logger = logger;
+            var url = "https://functionapp2018071101324.blob.core.windows.net/data/matches_latest.json";
+            _matches.Matches = _download_serialized_json_data<List<Match>>(url); 
         }
 
+        private static T _download_serialized_json_data<T>(string url) where T : new() {
+            //universal method, which takes url as arg and tries to convert json from url to class, which is specified at method call. 
+            //SIIRRÄ MUUALLE; CONTROLLERIIN PREF
+            using (var w = new WebClient()) {
+                var json_data = string.Empty;
+                // attempt to download JSON data as a string
+                try {
+                    json_data = w.DownloadString(url);
+                    //Console.Write(json_data);
+                }
+                catch (Exception) {
+                    //Use JSON from resources
+                    Console.WriteLine("JSON not loaded!");
+                }
+                // if string with JSON data is not empty, deserialize it to class and return its instance 
+                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
+            }
+        }  
+
+        //GET /footballmatch
         [HttpGet]
         public ActionResult GetAllMatches(){
-            return Ok();
+            return Ok(_matches);
         }
-        
+
+        //GET footballmatch/ID
+        //[HttpGet("{id}")]    
     }
 }
--- FootballAPI/Program.cs
+++ FootballAPI/Program.cs
@@ -17,9 +17,6 @@ namespace FootballAPI
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
-            var url = "https://functionapp2018071101324.blob.core.windows.net/data/matches_latest.json";
-            Matchlist _matches = new Matchlist();
-            _matches.Matches = _download_serialized_json_data<List<Match>>(url);     
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -27,24 +24,6 @@ namespace FootballAPI
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
-
-        private static T _download_serialized_json_data<T>(string url) where T : new() {
-            //universal method, which takes url as arg and tries to convert json from url to class, which is specified at method call. 
-            //SIIRRÄ MUUALLE; CONTROLLERIIN PREF
-            using (var w = new WebClient()) {
-                var json_data = string.Empty;
-                // attempt to download JSON data as a string
-                try {
-                    json_data = w.DownloadString(url);
-                    //Console.Write(json_data);
-                }
-                catch (Exception) {
-                    Console.WriteLine("JSON not loaded!");
-                }
-                // if string with JSON data is not empty, deserialize it to class and return its instance 
-                return !string.IsNullOrEmpty(json_data) ? JsonConvert.DeserializeObject<T>(json_data) : new T();
-            }
-        }          
+                });        
     }
 }
